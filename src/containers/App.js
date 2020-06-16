2e352e84fc7fb1d722ac9098ebe457e1880f93d3
DouglasKyrius <douglaskyrius@gmail.com>
2020-06-16 00:03:19 -0400

    inital files to repo

---
+++ src/containers/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import Header from '../components/Header';
+import Card from '../components/Card';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <Card />
+      </div>
+    );
+  }
+}
+
+export default App;
