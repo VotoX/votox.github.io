80c672f59fd44afdf1a84f3179876256248bfaf2
DouglasKyrius <douglaskyrius@gmail.com>
2020-06-16 02:30:29 -0400

    files

---
+++ precache-manifest.bd04c15e31cfe1178f83c3d61bf1c80b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24b46ffedd2621349cad12c78b33bf38",
+    "url": "/index.html"
+  },
+  {
+    "revision": "919ceb773317cd241c1e",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "e160e5a7061c5e47d634",
+    "url": "/static/css/main.41199717.chunk.css"
+  },
+  {
+    "revision": "919ceb773317cd241c1e",
+    "url": "/static/js/2.22c1a0f4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.22c1a0f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e160e5a7061c5e47d634",
+    "url": "/static/js/main.1fc853b5.chunk.js"
+  },
+  {
+    "revision": "08d073c89d9d6feaf8c6",
+    "url": "/static/js/runtime-main.f9de8e46.js"
+  }
+]);
